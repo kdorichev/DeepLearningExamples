a238a9f1a619deed7e9be0a0ea418d162be690b0
Konstantin Dorichev <korichev@gmail.com>
2020-08-06 17:12:06 +0300

    Replaced with text2speech/common/text/symbols.py
    Signed-off-by: Konstantin Dorichev <korichev@gmail.com>

--- PyTorch/SpeechSynthesis/Tacotron2/tacotron2/text/symbols.py
+++ PyTorch/SpeechSynthesis/Tacotron2/tacotron2/text/symbols.py
@@ -1,18 +1,21 @@
-""" from https://github.com/keithito/tacotron """
+# Adapted from
+# https://github.com/NVIDIA/DeepLearningExamples/tree/master/PyTorch/SpeechSynthesis/FastPitch
 
-'''
-Defines the set of symbols used in text input to the model.
+"""Defines the set of symbols used in text input to the model.
 
-The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
-from tacotron2.text import cmudict
+The default is a set of ASCII characters that works well for English or text
+that has been run through Unidecode. For other data, you can modify _characters.
+See TRAINING_DATA.md for details.
+
+from https://github.com/keithito/tacotron
+"""
+
+from common.text import cmudict
 
 _pad        = '_'
-_punctuation = '!\'(),.:;? '
-_special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+_punctuation = '!?,.:;– '
+_special = '+-'
+_letters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 
-# Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-_arpabet = ['@' + s for s in cmudict.valid_symbols]
-
-# Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters)  # + _arpabet
+pad_idx = 0
