3b52d74d8fc694e97286c3f1d67c2c607570b22d
Konstantin Dorichev <kdorichev@gmail.com>
2020-08-13 14:31:55 +0300

    Adapt for Russian

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/prepare_mels.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/prepare_mels.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-DATADIR="LJSpeech-1.1"
+DATADIR="Voituk_Narrative"
 FILELISTSDIR="filelists"
 
-TESTLIST="$FILELISTSDIR/ljs_audio_text_test_filelist.txt"
-TRAINLIST="$FILELISTSDIR/ljs_audio_text_train_filelist.txt"
-VALLIST="$FILELISTSDIR/ljs_audio_text_val_filelist.txt"
+#TESTLIST="$FILELISTSDIR/ljs_audio_text_test_filelist.txt"
+TRAINLIST="$FILELISTSDIR/Voituk_Narrative_train_filelist.txt"
+VALLIST="$FILELISTSDIR/Voituk_Narrative_valid_filelist.txt"
 
-TESTLIST_MEL="$FILELISTSDIR/ljs_mel_text_test_filelist.txt"
-TRAINLIST_MEL="$FILELISTSDIR/ljs_mel_text_train_filelist.txt"
-VALLIST_MEL="$FILELISTSDIR/ljs_mel_text_val_filelist.txt"
+#TESTLIST_MEL="$FILELISTSDIR/ljs_mel_text_test_filelist.txt"
+TRAINLIST_MEL="$FILELISTSDIR/Voituk_Narrative_mel_dur_pitch_train_filelist.txt"
+VALLIST_MEL="$FILELISTSDIR/Voituk_Narrative_mel_dur_pitch_valid_filelist.txt"
 
 mkdir -p "$DATADIR/mels"
-if [ $(ls $DATADIR/mels | wc -l) -ne 13100 ]; then
-    python preprocess_audio2mel.py --wav-files "$TRAINLIST" --mel-files "$TRAINLIST_MEL"
-    python preprocess_audio2mel.py --wav-files "$TESTLIST" --mel-files "$TESTLIST_MEL"
-    python preprocess_audio2mel.py --wav-files "$VALLIST" --mel-files "$VALLIST_MEL"	
-fi	
+#if [ $(ls $DATADIR/mels | wc -l) -ne 13100 ]; then
+python preprocess_audio2mel.py -d Voituk_Narrative --wav-files "$TRAINLIST" --mel-files "$TRAINLIST_MEL"
+#    python preprocess_audio2mel.py --wav-files "$TESTLIST" --mel-files "$TESTLIST_MEL"
+python preprocess_audio2mel.py -d Voituk_Narrative --wav-files "$VALLIST" --mel-files "$VALLIST_MEL"	
+#fi	
