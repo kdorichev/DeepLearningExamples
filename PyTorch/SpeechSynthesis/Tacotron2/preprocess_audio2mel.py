779d7bee460aa293a9ce0ce49a7f292ab20316f6
Konstantin Dorichev <korichev@gmail.com>
2020-09-22 16:33:36 +0300

    __doc__ and enumerate
    Signed-off-by: Konstantin Dorichev <korichev@gmail.com>

--- PyTorch/SpeechSynthesis/Tacotron2/preprocess_audio2mel.py
+++ PyTorch/SpeechSynthesis/Tacotron2/preprocess_audio2mel.py
@@ -1,4 +1,5 @@
 import argparse
+from argparse import ArgumentParser
 import torch
 
 from tacotron2.data_function import TextMelLoader
@@ -39,21 +40,30 @@ def parse_args(parser):
     return parser
 
 
-def audio2mel(dataset_path, audiopaths_and_text, melpaths_and_text, args):
+def audio2mel(dataset_path: str, audiopaths_and_text: str, melpaths_and_text: str,
+              args: ArgumentParser)-> None:
+    """Create mel spectrograms on disk from audio files.
+
+    Args:
+        dataset_path (str): Path to dataset
+        audiopaths_and_text (str): Path to filelist with audio paths and text
+        melpaths_and_text (str): Path to filelist with mel paths and text
+        args (ArgumentParser): Namespace with arguments
+    """
 
     melpaths_and_text_list = load_filepaths_and_text(dataset_path, melpaths_and_text)
     audiopaths_and_text_list = load_filepaths_and_text(dataset_path, audiopaths_and_text)
     data_loader = TextMelLoader(dataset_path, audiopaths_and_text, args)
 
-    for i in range(len(melpaths_and_text_list)):
+    for i, melpath_and_text in enumerate(melpaths_and_text_list):
         if i%100 == 0:
             print("done", i, "/", len(melpaths_and_text_list))
         mel = data_loader.get_mel(audiopaths_and_text_list[i][0])
-        torch.save(mel, melpaths_and_text_list[i][0])
+        torch.save(mel, melpath_and_text[0])
 
 def main():
 
-    parser = argparse.ArgumentParser(description='PyTorch Tacotron 2 Training')
+    parser = argparse.ArgumentParser(description='PyTorch Tacotron2 Training')
     parser = parse_args(parser)
     args = parser.parse_args()
 
