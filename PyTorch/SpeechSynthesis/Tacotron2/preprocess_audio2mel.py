acb97f343028f512a6afe3cf2431eee9fabbd76d
Konstantin Dorichev <kdorichev@gmail.com>
2021-01-04 20:04:44 +0300

    Defauld values for argiments added

--- PyTorch/SpeechSynthesis/Tacotron2/preprocess_audio2mel.py
+++ PyTorch/SpeechSynthesis/Tacotron2/preprocess_audio2mel.py
@@ -17,25 +17,25 @@ def parse_args(parser):
                         type=str, help='Path to filelist with mel paths and text')
     parser.add_argument('--text-cleaners', nargs='*',
                         default=['russian_cleaner2'], type=str,
-                        help='Type of text cleaners for input text')
+                        help='Type of text cleaners for input text. Default: russian_cleaner2')
     parser.add_argument('--max-wav-value', default=1.0, type=float,
-                        help='Maximum audiowave value')
+                        help='Maximum audiowave value to divide on for normalization')
     parser.add_argument('--sampling-rate', default=22050, type=int,
-                        help='Sampling rate')
+                        help='Sampling rate. Default: 22050')
     parser.add_argument('--filter-length', default=1024, type=int,
-                        help='Filter length')
+                        help='Filter length. Default: 1024')
     parser.add_argument('--hop-length', default=256, type=int,
-                        help='Hop (stride) length')
+                        help='Hop (stride) length. Default: 256')
     parser.add_argument('--win-length', default=1024, type=int,
-                        help='Window length')
+                        help='Window length. Default: 1024')
     parser.add_argument('--mel-fmin', default=0.0, type=float,
-                        help='Minimum mel frequency')
+                        help='Minimum mel frequency. Default: 0.0')
     parser.add_argument('--mel-fmax', default=8000.0, type=float,
-                        help='Maximum mel frequency')
+                        help='Maximum mel frequency. Default: 8000.0')
     parser.add_argument('--n-mel-channels', default=80, type=int,
-                        help='Number of bins in mel-spectrograms')
+                        help='Number of bins in mel-spectrograms. Default: 80')
     parser.add_argument('--load-mel-from-disk', action='store_true', default=False, 
-                        help='To load pre-calculated mels from disk')
+                        help='To load pre-calculated mels from disk. Default: False')
 
     return parser
 
