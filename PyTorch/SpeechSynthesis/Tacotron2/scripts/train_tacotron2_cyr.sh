8840928a56efbef0345fc87576bf09951571c94a
Konstantin Dorichev <kdorichev@gmail.com>
2021-03-17 18:10:56 +0300

    Updated  training script with variables

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/train_tacotron2_cyr.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/train_tacotron2_cyr.sh
@@ -1,1 +1,5 @@
-python -m multiproc train.py -o runs/test6 -d minidataset --cudnn-enable --cudnn-benchmark --text-cleaners 'russian_cleaner2' --training-files  minidataset/mel_dur_pitch_train_filelist.txt --validation-files minidataset/mel_dur_pitch_valid_filelist.txt -m Tacotron2 --epochs 1 --epochs-per-checkpoint 1 -lr 0.001 -bs 64 --gradient-accumulation-steps 10 --mel-fmax 12000 --log-dir runs/test6/nvlog.json --load-mel-from-disk
+TEST="test2"
+BS=128
+GA=1
+
+python -m multiproc train.py -o runs/"$TEST" -d minidataset --cudnn-enable --cudnn-benchmark --text-cleaners 'russian_cleaner2' --training-files  minidataset/mel_dur_pitch_train_filelist.txt --validation-files minidataset/mel_dur_pitch_valid_filelist.txt -m Tacotron2 --epochs 1 --epochs-per-checkpoint 1 -lr 0.001 -bs "$BS" --gradient-accumulation-steps "$GA" --mel-fmax 12000 --log-file runs/"$TEST"/nvlog.json --load-mel-from-disk --amp
