d7f92e6965104a1184a079d188b8750b979654c8
Konstantin Dorichev <kdorichev@gmail.com>
2021-04-06 09:19:00 +0300

    Variables added for convenience

--- PyTorch/SpeechSynthesis/Tacotron2/scripts/train_waveglow_cyr.sh
+++ PyTorch/SpeechSynthesis/Tacotron2/scripts/train_waveglow_cyr.sh
@@ -1,2 +1,7 @@
-python -m multiproc train.py -m WaveGlow -o ./runs/wg-test2 -lr 1e-4 --epochs 1 --epochs-per-checkpoint 1 --gradient-accumulation 1 --load-mel-from-disk -bs 10 --sampling-rate 24000 --segment-length 8000 --weight-decay 0 --grad-clip-thresh 3.4028234663852886e+38 --cudnn-enabled --cudnn-benchmark --log-file ./runs/wg-test2/nvlog.json -d minidataset --training-files minidataset/train_filelist.txt --validation-files minidataset/valid_filelist.txt --resume-from-last --mel-fmax 12000 
+TEST="wg-0323"
+DS="VN_40h"
+BS=1
+GA=4
+
+python -m multiproc train.py -m WaveGlow -o ./runs/"$TEST" -lr 1e-4 --epochs 10 --epochs-per-checkpoint 10 --gradient-accumulation "$GA" --load-mel-from-disk -bs "$BS" --sampling-rate 24000 --segment-length 6000 --weight-decay 0 --grad-clip-thresh 3.4028234663852886e+38 --cudnn-enabled --cudnn-benchmark --log-file ./runs/"$TEST"/nvlog.json -d "$DS" --training-files "$DS"/train_filelist.txt --validation-files "$DS"/valid_filelist.txt --resume-from-last --mel-fmax 12000 
 #python -m multiproc train.py -m WaveGlow -o ./output/ -lr 1e-4 --epochs 1501 -bs 4 --segment-length  8000 --weight-decay 0 --grad-clip-thresh 3.4028234663852886e+38 --cudnn-enabled --cudnn-benchmark --log-file nvlog.json
